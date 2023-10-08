34eb4797b4f8b6b8fbc38b7815b9c4ab31bf477b
Gerardo González García <spartano1994@hotmail.com>
2023-10-07 21:20:52 -0600

    Primer intento de menú desplegable

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ let flechaCerrar = document.querySelector(".barra-lateral-cerrar-img");
 
 flechaCerrar.addEventListener( "click" , cerrarBarraClick )
 
-window.addEventListener("resize", cerrarBarraDefecto)
+let barraLateral= document.querySelector("[data-aside]")
 
 function cerrarBarraDefecto( ){
     let barraLateral= document.querySelector("[data-aside]")
-    barraLateral.style.transitionProperty = "left";
-    barraLateral.style.transitionDuration = "0s";
-    barraLateral.style.left = "-80%";
+
+    
+    // barraLateral.style.transitionProperty = "left";
+    // barraLateral.style.transitionDuration = "0s";
+    // barraLateral.style.left = "-80%";
 }
 
 function cerrarBarraClick(e){
     let barraLateral= document.querySelector("[data-aside]");
-
-    barraLateral.style.transitionProperty = "left";
-    barraLateral.style.transitionDuration = "0.5s";
-    barraLateral.style.left = "-80%";
-    // barraLateral.style.display = "none";
+    
+    barraLateral.classList.remove( "barra-lateral-activar" )
 }
 
 /*---------------------------------------------------------------------*/
@@ -26,10 +25,8 @@ let menuAbrir = document.querySelector(".menu-img");
 
 menuAbrir.addEventListener( "click" , desplegarBarraClick );
 
-function desplegarBarraClick(){
+function desplegarBarraClick(e){
     let barraLateral= document.querySelector("[data-aside]")
-    barraLateral.style.transitionProperty = "left";
-    barraLateral.style.transitionDuration = "0.5s";
-    barraLateral.style.left = "0%";
-    // barraLateral.style.display = "none";
+
+    barraLateral.classList.add( "barra-lateral-activar" )
 }
